260ec4193445786faca71893950c7ca8b5c234c2
Pablo Toro <juan.p.toro@correounivalle.edu.co>
2026-03-12 14:20:39 -0500

    fix: python 3.11 + scheduler + panel

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ def create_app():
     app.register_blueprint(barberos_bp, url_prefix="/barberos")
     app.register_blueprint(bot_bp)
     app.register_blueprint(panel_bp)
-    iniciar_scheduler
+    iniciar_scheduler()
 
     
 
